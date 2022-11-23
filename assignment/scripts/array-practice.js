9d2c788b64d21b17f07b2abecc33e0053c22605a
Drew Hoek <drewh815@gmail.com>
2022-11-22 23:33:58 -0600

    Completed questions 1-3

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -1,44 +1,44 @@
-console.log('****** Array Practice *******');
+console.log( '****** Array Practice *******' );
 
 // 1. Creating arrays
-console.log('--- 1. Practice creating arrays ---');
+console.log( '--- 1. Practice creating arrays ---' );
 
 // Example: Array of animals
-let animalArray = ['fish', 'cat', 'bird', 'dog'];
-console.log('Animals are: ', animalArray);
+let animalArray = [ 'fish', 'cat', 'bird', 'dog' ];
+console.log( 'Animals are: ', animalArray );
 
 // 1.a. TODO: Make an array with some favorite foods
-
+let foodsArray = [ 'pizza', 'tacos', 'pasta', 'burgers' ]
 
 // 1.b. TODO: Log your array of foods to the console with a message, similar 
 //      to the example above
-
+console.log( 'Favorite foods are: ', foodsArray );
 
 
 // 2. Array.length - tells you how many items are in the array
 console.log('--- 2. Length of an array ---');
 
 // Example: How many animals are in the array? 
-console.log('Number of animals:', animalArray.length);
+console.log( 'Number of animals: ', animalArray.length );
 
 // 2.a. TODO: Log to the console the number of foods in your array
-
+console.log( 'Number of foods: ', foodsArray.length );
 
 // 3. Accessing array items
-console.log('--- 3. Accessing items in an array ---');
+console.log( '--- 3. Accessing items in an array ---' );
 
 // Example: Log the first animal from the array using it's array index
-console.log('First animal is', animalArray[0])
+console.log( 'First animal is', animalArray[ 0 ] );
 
 // 3.a. TODO: Log the second animal in the array 
-
+console.log( 'Second animal is', animalArray[ 1 ] );
 
 // 3.b. TODO: Log the last animal in the array using it's array index 
-
+console.log( 'Last animal is', animalArray[ 3 ] );
 
 // 3.c. (STRETCH) TODO: Log the last animal by using the array length, 
 //      instead of the exact index number of the last item
-
+console.log( 'Last animal is', animalArray[ animalArray.length - 1 ], '(STRETCH)' );
 
 
 // 4. Adding & Removing Array Items
